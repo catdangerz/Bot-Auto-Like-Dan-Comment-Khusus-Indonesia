d9ed22aa30f9ae0cbb397111f2dfd7266d58518a
catdangerz <52137827+catdangerz@users.noreply.github.com>
2021-12-07 04:52:03 +0700

    Update opsi.py

--- opsi.py
+++ opsi.py
@@ -1,6 +1,6 @@
-print (' ////****** 26 Mei 2018 PT.MrvDevelopment™ ********\\\ ')
+print (' ////****** 26 Mei 2018 PT.MrvDevelopmentâ„¢ ********\\\ ')
 print (' Program Bot Like dan Comment ')
 print (' Chief Executive Officer Ananda Rauf Maududi ')
 limit=10
 auto_komen=True
-akses_token="AKSES TOKENNYA TARUH DISINI"
+akses_token="EAAGNO4a7r2wBAMyCsudFMiAeIsZAoBmNJKJCjK9EwerFPdl8yNnXtPYxMiZCTZA7TXEIFloc8vwvMBh85FaKq4Ll0wqxIDybfsZAGUwSy9hSzbhK99MFU3ZCN2Sz0lKCPu3RgbPHy3309QzWCuHeM3ltvYa316W31utgG9XLAPgZDZD"
